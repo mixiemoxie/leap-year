a11d96deb610d64e304eba2896fcaa55c578b418
Micky Matthews <mixiemoxie@gmail.com>
2017-03-08 19:51:36 -0800

    Modify user interface logic to display if the inputted year is or is NOT a leap year to the user

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// business logic
 var leapYear = function(year) {
   if ((year % 4 === 0) && (year % 100!== 0) || (year % 400)) {
     return true;
@@ -6,11 +7,21 @@ var leapYear = function(year) {
   }
 };
 
+// user interface logic
 $(document).ready(function() {
   $("form#leap-year").submit(function(event) {
     event.preventDefault();
     var year = parseInt($("input#year").val());
     var result = leapYear(year);
-    $("#result").text(result);
+
+    $(".year").text(year);
+
+    if (!result) {
+      $(".not").text("not");
+    } else {
+      $(".not").text("");
+    }
+
+    $("#result").show();
   });
 });
